369c71ac5a972f4c7920855c601523639850817b
Technological-Singularity <83087706+Technological-Singularity@users.noreply.github.com>
2022-08-15 21:41:50 -0700

    Add project files.

---
+++ Charon - Minifix - Blue Collar/Plugin.cs
@@ -0,0 +1,25 @@
+﻿using BepInEx;
+using HarmonyLib;
+
+namespace Charon_SV_Minifix {
+    [BepInPlugin(pluginGuid, pluginName, pluginVersion)]
+    [BepInProcess("Star Valor.exe")]
+    public class Plugin : BaseUnityPlugin {
+        public const string pluginGuid = "starvalor.charon.minifix.bluecollar";
+        public const string pluginName = "Charon - Minifix - Blue Collar";
+        public const string pluginVersion = "0.0.0.0";
+
+        public void Awake() {
+            Harmony.CreateAndPatchAll(typeof(Plugin));
+        }
+
+        [HarmonyPatch(typeof(CharacterScreen), nameof(CharacterScreen.Open))]
+        [HarmonyPostfix]
+        public static void Open(int mode) {
+            if (PChar.HasPerk(3)) { //White Collar
+                PChar.RemovePerk(3);
+                PerkDB.AcquirePerk(1); //Miner
+            }
+        }
+    }
+}
