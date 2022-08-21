ecc642163845e7f5c98a4545ecb386b7b794806f
Technological-Singularity <83087706+Technological-Singularity@users.noreply.github.com>
2022-08-21 16:23:15 -0700

    Dependency changes

---
+++ Charon - Minifix - Wider Targeting - Copy/Plugin.cs
@@ -0,0 +1,19 @@
+﻿using BepInEx;
+using HarmonyLib;
+using UnityEngine;
+
+namespace Charon.StarValor {
+    [BepInPlugin(pluginGuid, pluginName, pluginVersion)]
+    [BepInProcess("Star Valor.exe")]
+    public class Plugin : BaseUnityPlugin {
+        public const string pluginGuid = "starvalor.charon.GUID";
+        public const string pluginName = "Charon - PLUGINNAME";
+        public const string pluginVersion = "0.0.0.0";
+        static BepInEx.Logging.ManualLogSource Log;
+
+        public void Awake() {
+            Log = Logger;
+            Harmony.CreateAndPatchAll(typeof(Plugin));
+        }       
+    }
+}
